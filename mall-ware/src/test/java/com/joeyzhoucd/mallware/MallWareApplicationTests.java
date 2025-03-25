30856aa36dc446c2af3e4af05751a436703c42ce
joey.zhou.cd <joey.zhou@guitarcenter.com>
2025-03-24 19:37:43 -0700

    Initialize project structure

---
+++ mall-ware/src/test/java/com/joeyzhoucd/mallware/MallWareApplicationTests.java
@@ -0,0 +1,13 @@
+package com.joeyzhoucd.mallware;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MallWareApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
