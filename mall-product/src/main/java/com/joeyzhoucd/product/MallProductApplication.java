cf4e8d953a941efcfbb6d1e9a9f972b00fc072a4
joey.zhou.cd <joey.zhou@guitarcenter.com>
2025-04-03 01:03:37 -0700

    integrated open feign, Update the cloud.alibaba.version from 2.2.7.RELEASE to 2021.0.5.0, this ensures Nacos uses Spring Cloud LoadBalancer instead of Ribbon.

--- mall-product/src/main/java/com/joeyzhoucd/product/MallProductApplication.java
+++ mall-product/src/main/java/com/joeyzhoucd/product/MallProductApplication.java
@@ -1,11 +1,15 @@
 package com.joeyzhoucd.product;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
 @EnableDiscoveryClient
+@EnableFeignClients(basePackages = "com.joeyzhoucd.product.feign")
+@MapperScan("com.joeyzhoucd.product.dao")
 public class MallProductApplication {
 
     public static void main(String[] args) {
